5f9ba8f8ef88e5cf3cef771a94e021891d9fbd95
Rania Khoury <rania.kh3107@gmail.com>
2024-10-09 19:33:41 +0300

    divide crawled urls into several parquet files

--- mycrawler/mycrawler/spiders/sub_links_spider.py
+++ mycrawler/mycrawler/spiders/sub_links_spider.py
@@ -2,6 +2,7 @@ import scrapy
 from urllib.parse import urljoin, urlparse, urlunparse
 import pandas as pd
 from collections import defaultdict
+import os
 
 
 class SublinkSpider(scrapy.Spider):
@@ -14,7 +15,8 @@ class SublinkSpider(scrapy.Spider):
         # self.result = {}
         self.sublinks_list = []
         self.visited_urls = set()
-        self.url_counter = defaultdict(int) # To count how many times a base URL is crawled with different params
+        self.file_counter = 1
+        self.url_counter = defaultdict(int)  # To count how many times a base URL is crawled with different params
 
     def parse(self, response, depth=1):
         seed_url = response.url
@@ -45,7 +47,8 @@ class SublinkSpider(scrapy.Spider):
                     sub_links.add(full_link_without_fragment)
 
                     # Schedule a request for the sublink with depth+1
-                    yield scrapy.Request(full_link_without_fragment, callback=self.parse, cb_kwargs={'depth': depth+1})
+                    yield scrapy.Request(full_link_without_fragment, callback=self.parse,
+                                         cb_kwargs={'depth': depth + 1})
 
                 # # save only 1 depth down
                 # path_parts = parsed_full_link.path.strip('/').split('/')
@@ -63,20 +66,36 @@ class SublinkSpider(scrapy.Spider):
 
         self.sublinks_list.extend(list(sub_links))
 
+        if len(self.sublinks_list) >= 100000:
+            self.save_to_parquet()
+            self.sublinks_list = []
+
         yield {'seed_url': seed_url, 'sub_links': list(sub_links)}
+
+    def save_to_parquet(self):
+        os.makedirs("crawleroutput/parquet", exist_ok=True)
+        df_sublinks = pd.DataFrame({"sublinks": self.sublinks_list})
+        df_sublinks.to_parquet(f"crawleroutput/parquet/sublinks_7_part_{self.file_counter}.parquet", index=False)
+        print(f"Part {self.file_counter} saved.")
+        self.file_counter += 1
 
     def errback(self, failure):
         self.failed_urls += 1
         self.logger.error(repr(failure))
         self.logger.error(f'Failed URL: {failure.request.url}')
 
+        with open("failed_urls.txt", "a") as f:
+            f.write(failure.request.url + "\n")
+
     def closed(self, reason):
         # Print the results and number of failed URLs
-        print(f'Number of failed URLs: {self.failed_urls}')
         # df_results = pd.DataFrame(list(self.result.items()), columns=['seed_url', 'sub_links'])
         # df_results['sub_links'] = df_results['sub_links'].apply(lambda x: ','.join(x))
         # df_results.to_csv('crawleroutput/results_seed_subs_dict_7.csv', index=False)
         # print("Results have been written to results_seed_subs_dict_7.csv")
-        df_sublinks = pd.DataFrame({"sublinks": self.sublinks_list})
-        df_sublinks.to_csv("crawleroutput/sublinks_7.csv", index=False)
-        print("Results have been written to sublinks_7.csv")
+        # df_sublinks = pd.DataFrame({"sublinks": self.sublinks_list})
+        # df_sublinks.to_csv("crawleroutput/sublinks_7.csv", index=False)
+        # print("Results have been written to sublinks_7.csv")
+        if self.sublinks_list:
+            self.save_to_parquet()
+        print(f'Number of failed URLs: {self.failed_urls}')
